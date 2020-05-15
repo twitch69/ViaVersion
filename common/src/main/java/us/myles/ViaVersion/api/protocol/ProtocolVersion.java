4da639278e4d6a4f71518a8a0d4cad2f1d85b9ef
twitch69 <62627312+twitch69@users.noreply.github.com>
2020-05-15 17:39:07 -0400

    Update ProtocolVersion.java

--- common/src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
+++ common/src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
@@ -43,6 +43,7 @@ public class ProtocolVersion {
     public static final ProtocolVersion v1_15;
     public static final ProtocolVersion v1_15_1;
     public static final ProtocolVersion v1_15_2;
+    public static final ProtocolVersion v20w20b;
     public static final ProtocolVersion unknown;
 
     private final int id;
@@ -82,6 +83,7 @@ public class ProtocolVersion {
         register(v1_15 = new ProtocolVersion(573, "1.15"));
         register(v1_15_1 = new ProtocolVersion(575, "1.15.1"));
         register(v1_15_2 = new ProtocolVersion(578, "1.15.2"));
+        register(v20w20b = new ProtocolVersion(717, "20w20b"));
         register(unknown = new ProtocolVersion(-1, "UNKNOWN"));
     }
 
